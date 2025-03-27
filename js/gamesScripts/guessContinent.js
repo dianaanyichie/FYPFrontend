6611dfcdd8c7230b6af88499c923b8b1a8848356
dianaanyichie <dianaanyichie9@gmail.com>
2025-03-27 22:33:01 +0000

    Guess the Continent

---
+++ js/gamesScripts/guessContinent.js
@@ -0,0 +1,132 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js"
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { getFirestore, setDoc, getDoc, doc } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBZ5lwJwOTqA6ne9O4p1Ey47ZnYrXm-WyQ",
+  authDomain: "littleguardiansdata.firebaseapp.com",
+  projectId: "littleguardiansdata",
+  storageBucket: "littleguardiansdata.firebasestorage.app",
+  messagingSenderId: "409967816932",
+  appId: "1:409967816932:web:abb5610b2a90cd6957fcf1",
+  measurementId: "G-9G3SKPN7X1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+const db = getFirestore();
+
+async function endGame() {
+
+    const popupContainer = document.getElementById('popup-container');
+    const overlay = document.querySelector('.overlay');
+    popupContainer.classList.remove('hidden');
+    popupContainer.style.display = 'block';
+    overlay.style.display = 'block';
+
+    popupContainer.innerHTML = ''; // Clear existing content
+
+    const message = document.createElement('p');
+    message.textContent = `Game over! Your score: ${score}`;
+
+    const playAgainButton = document.createElement('button');
+    playAgainButton.textContent = 'Play Again?';
+    playAgainButton.onclick = () => {
+        score = 0;
+        time = 60;
+        document.getElementById("score").textContent = `Score: ${score}`;
+        document.getElementById('option1').disabled = false;
+        document.getElementById('option2').disabled = false;
+        loadNewAnimal();
+        startTimer();
+        popupContainer.classList.add('hidden');
+        popupContainer.style.display = 'none';
+        overlay.style.display = 'none';
+    };
+
+    const otherGamesButton = document.createElement('button');
+    otherGamesButton.textContent = 'Play Other Games';
+    otherGamesButton.onclick = () => {
+        window.location.href = '../gamesPage.html';
+    };
+
+    popupContainer.appendChild(message);
+    popupContainer.appendChild(playAgainButton);
+    popupContainer.appendChild(otherGamesButton);
+
+    await updateHighScore(score);
+    await updateGamePlayCount();
+}
+
+async function updateHighScore(currentScore) {
+    
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (!user) {
+        console.error("No user is currently signed in.");
+        return;
+    }
+
+    const userRef = doc(db, "users", user.uid);
+    const userDoc = await getDoc(userRef);
+
+    if (userDoc.exists()) {
+        const userData = userDoc.data();
+        const highScore = userData.guessContinentHighScore || 0;
+
+        if (!userData.hasOwnProperty('guessContinentHighScore')) {
+            await setDoc(userRef, { guessContinentHighScore: currentScore }, { merge: true });
+            console.log('High score field initialized and score saved:', currentScore);
+        } else if (currentScore > highScore) {
+            
+            await setDoc(userRef, { guessContinentHighScore: currentScore }, { merge: true });
+            console.log('New high score saved:', currentScore);
+        }
+    } else {
+        console.error("No user data found!");
+    }
+}
+
+async function updateGamePlayCount() {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (!user) {
+        console.error("No user is currently signed in.");
+        return;
+    }
+
+    const userRef = doc(db, "users", user.uid);
+    const userDoc = await getDoc(userRef);
+
+    if (userDoc.exists()) {
+        const userData = userDoc.data();
+        const currentCount = userData.guessAnimalCount || 0;
+
+        const newCount = currentCount + 1;
+        await setDoc(userRef, { guessAnimalCount: newCount }, { merge: true });
+        console.log('Game play count updated:', newCount);
+    } else {
+        console.error("No user data found!");
+    }
+}
+
+window.addEventListener('load', () => {
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+});
